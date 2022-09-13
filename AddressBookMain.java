b8b63004ea0b335779fdb5e31497c612b53bc1af
Saqlain <iamkhatik786@gmail.com>
2022-09-13 19:31:29 +0530

    Welcome message

---
+++ AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.addressbook.controller;
+
+public class AddressBookMain {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("*****Welcome to My Address Book*****");
+	}
+
+}
