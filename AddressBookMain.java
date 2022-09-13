5c5c6aca98735d215e0c2207610ba9f017993f9c
Saqlain <iamkhatik786@gmail.com>
2022-09-13 19:59:22 +0530

    Create A Contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,10 +1,68 @@
 package com.addressbook.controller;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("*****Welcome to My Address Book*****");
+		
+		Character ch = 'a';
+		Scanner sc = new Scanner(System.in);
+		
+		AddressBookServices create = new AddressBookServImpl();
+		create.createContact();
+		
+		System.out.println("Created Contact  : ");
+		create.displayContact();
 	}
 
+}
+
+interface AddressBookServices {
+	
+	public void createContact();
+	public void displayContact();
+}
+
+class AddressBookServImpl implements AddressBookServices {
+	
+	Scanner sc = new Scanner(System.in);
+	Contacts con = new Contacts();
+	
+	@Override
+	public void createContact() {
+	
+		System.out.println("Enter First Name : ");
+		con.setFirstN(sc.nextLine());
+		
+		System.out.println("Enter Last Name : ");
+		con.setLastN(sc.nextLine());
+		
+		System.out.println("Address : ");
+		con.setAddress(sc.nextLine());
+		
+		System.out.println("City : ");
+		con.setCity(sc.nextLine());
+		
+		System.out.println("State : ");
+		con.setState(sc.nextLine());
+		
+		System.out.println("E-mail : ");
+		con.setEmail(sc.nextLine());
+		
+	}
+	
+	@Override
+	public void displayContact() {
+		
+		System.out.println("First Name : " + con.getFirstN());
+		System.out.println("Last Name : " + con.getLastN());
+		System.out.println("Address : " + con.getAddress());
+		System.out.println("City : " + con.getCity());
+		System.out.println("State : " + con.getState());
+		System.out.println("E-mail : " + con.getEmail());
+
+	}
 }
---
+++ Contacts.java
@@ -0,0 +1,56 @@
+package com.addressbook.controller;
+
+public class Contacts {
+		
+		String firstN, lastN, address, city, state, email;
+		int zip, phN;
+		
+		public String getFirstN() {
+			return firstN;
+		}
+		public void setFirstN(String firstN) {
+			this.firstN = firstN;
+		}
+		public String getLastN() {
+			return lastN;
+		}
+		public void setLastN(String lastN) {
+			this.lastN = lastN;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		public String getState() {
+			return state;
+		}
+		public void setState(String state) {
+			this.state = state;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public int getZip() {
+			return zip;
+		}
+		public void setZip(int zip) {
+			this.zip = zip;
+		}
+		public int getPhN() {
+			return phN;
+		}
+		public void setPhN(int phN) {
+			this.phN = phN;
+		}	
+	}
